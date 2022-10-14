c0b503722f564867855706c53759c2e9cbe2b823
Kyle Joseph Timajo <timajokyle24@gmail.com>
2022-10-14 15:50:19 +0800

    feat: set skill component cursor to pointer

--- src/components/skill.jsx
+++ src/components/skill.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Skill = ({ Icon, name }) => {
   return (
-    <div className="bg-[#1D2C45] rounded-2xl p-3 hover:shadow-xl">
+    <div className="bg-[#1D2C45] rounded-2xl p-3 hover:shadow-xl cursor-pointer">
       <Icon size={"80%"} className="text-white mx-auto mb-0 sm:mb-1" />
       <p className="text-center text-xs sm:text-sm font-normal md:font-medium">
         {name}
