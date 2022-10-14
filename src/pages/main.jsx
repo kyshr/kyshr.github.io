4e1fed39d6ed5fab9bfee6509dd1e418e24a06ad
Kyle Joseph Timajo <timajokyle24@gmail.com>
2022-10-14 15:35:15 +0800

    refactor: removed react router dom

--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -1,17 +1,12 @@
 import Layout from "../components/layout";
 import HomePage from "./home/home";
-import { HashRouter, Routes, Route } from "react-router-dom";
 
 const MainWrapper = () => {
   return (
     <div className="main-wrapper">
-      <HashRouter basename="/">
-        <Layout>
-          <Routes>
-            <Route exact path="/" element={<HomePage />} />
-          </Routes>
-        </Layout>
-      </HashRouter>
+      <Layout>
+        <HomePage />
+      </Layout>
     </div>
   );
 };
