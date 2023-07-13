d82f27f7bd780d8098c54f52f9153e63f1f4a650
Kyle Joseph Timajo <timajokyle24@gmail.com>
2023-07-13 18:10:15 +0800

    feat: update skill component

--- src/components/skill.jsx
+++ src/components/skill.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 
 const Skill = ({ Icon, name }) => {
-  return (
-    <div className="bg-[#1D2C45] rounded-2xl p-3 hover:shadow-xl cursor-pointer">
-      <Icon size={"80%"} className="text-white mx-auto mb-0 sm:mb-1" />
-      <p className="text-center text-[9px] sm:text-sm font-normal md:font-medium">
-        {name}
-      </p>
-    </div>
-  );
+    return (
+        <div className="bg-[#1D2C45] rounded-2xl p-3 hover:scale-105 transition-all ease-in-out cursor-pointer">
+            <Icon size={"80%"} className="text-white mx-auto mb-0 sm:mb-1" />
+            <p className="text-center text-[9px] sm:text-sm font-normal">
+                {name}
+            </p>
+        </div>
+    );
 };
 
 export default Skill;
