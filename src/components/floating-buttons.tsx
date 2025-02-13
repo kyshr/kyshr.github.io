da3a3fed5804a52d61a5883c34c372644085c02e
Kyle Joseph Timajo <timajokyle24@gmail.com>
2025-02-13 13:19:11 +0800

    chore: update floating button bg color

--- src/components/floating-buttons.tsx
+++ src/components/floating-buttons.tsx
@@ -6,8 +6,8 @@ const FloatingButtons = () => {
     <div className="space-y-4">
       <div>
         <a title="github" href="https://github.com/kyshr" target="_blank" rel="noopener noreferrer">
-          <div className="w-11 h-11 flex items-center justify-center bg-white rounded-full p-1.5">
-            <IoLogoGithub size={32} />
+          <div className="w-11 h-11 flex items-center justify-center bg-sky-300 bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 rounded-full p-1.5">
+            <IoLogoGithub className="text-white" size={32} />
           </div>
         </a>
       </div>
@@ -17,8 +17,8 @@ const FloatingButtons = () => {
           href="https://www.linkedin.com/in/kyle-joseph-timajo-959b95233/"
           target="_blank"
           rel="noopener noreferrer">
-          <div className="w-11 h-11 flex items-center justify-center bg-white rounded-full p-1.5">
-            <IoLogoLinkedin size={32} />
+          <div className="w-11 h-11 flex items-center justify-center bg-sky-300 bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 rounded-full p-1.5">
+            <IoLogoLinkedin className="text-white" size={32} />
           </div>
         </a>
       </div>
